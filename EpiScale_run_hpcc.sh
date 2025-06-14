f5f7b598bdc83f279845415eb07ddd2eabe8a993
EthanNowaski <enowa001@ucr.edu>
2025-06-13 18:04:52 -0700

    Update EpiScale_run_hpcc.sh
    Specifying gpu core to be compatible with updated cuda version

--- EpiScale_run_hpcc.sh
+++ EpiScale_run_hpcc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --ntasks=1
 #SBATCH --mem=90G
 #SBATCH --output=compileTest# This affects the print out of the "std::cout" in the script, make sure this is changed for different jobs.
